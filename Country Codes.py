74ccfb5d3ab2505d259990f8b6ee0d5b0bbc705c
Amreet711 <sunny1935@gmail.com>
2026-09-03 19:14:11 +1000

    Country Codes

---
+++ Country Codes.py
@@ -0,0 +1,5 @@
+country_codes={"India":1011,"Australia":3030,"Nepal":1505}
+print(country_codes.get("India"))
+print(country_codes["Australia"])
+print(country_codes.get("Nepal"))
+print(country_codes.get("Dubai","not found"))
